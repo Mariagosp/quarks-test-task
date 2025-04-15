a51924956dd4a51ef961dd9ceb8b8ff726120797
Maria <m.gosp1704@gmail.com>
2025-04-15 11:56:37 +0300

    fixed checkboxes

--- src/components/CustomSelect/CustomSelect.tsx
+++ src/components/CustomSelect/CustomSelect.tsx
@@ -1,14 +1,18 @@
 import React from "react";
-import "./CustomSelect.scss"; 
-import { Option } from '../../types/Option';
+import "./CustomSelect.scss";
+import { Option } from "../../types/Option";
 
 type Props = {
   options: Option[];
   selectedOptions: string[];
   handleSelectChange: (value: string) => void;
-}
+};
 
-const CustomSelect: React.FC<Props> = ({ options, selectedOptions, handleSelectChange }) => {
+const CustomSelect: React.FC<Props> = ({
+  options,
+  selectedOptions,
+  handleSelectChange,
+}) => {
   return (
     <>
       <div className="select">
@@ -16,24 +20,32 @@ const CustomSelect: React.FC<Props> = ({ options, selectedOptions, handleSelectC
           {options.map((option) => {
             const isChecked = selectedOptions.includes(option.value);
             return (
-              <div key={option.value} className={isChecked ? 'select__options--option checked' : 'select__options--option'}>
-              <label className="select__options--option--label" htmlFor={option.value}>
-                <span className="select__options--option--label--emoji">
-                  {option.emoji}
-                </span>
-                <span className="select__options--option--label--name">
-                  {option.label}
-                </span>
-              </label>
-              <input
-                className="checkbox"
-                type="checkbox"
-                id={option.value}
-                checked={isChecked}
-                onChange={() => handleSelectChange(option.value)}
-              />
-            </div>
-            )
+              <div
+                key={option.value}
+                className={
+                  isChecked
+                    ? "select__options--option checked"
+                    : "select__options--option"
+                }
+              >
+                <label className="select__options--option--label">
+                  <div className="select__options--option--label--name">
+                    <span className="select__options--option--label--name--emoji">
+                      {option.emoji}
+                    </span>
+                    <span className="select__options--option--label--name--text">
+                      {option.label}
+                    </span>
+                  </div>
+                  <input
+                    className="checkbox"
+                    type="checkbox"
+                    checked={isChecked}
+                    onChange={() => handleSelectChange(option.value)}
+                  />
+                </label>
+              </div>
+            );
           })}
         </div>
       </div>
